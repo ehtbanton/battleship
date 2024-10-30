f27bf56981d6d9e7d6770a7d9a0fa8334ad20857
ehtbanton <notnatheyam@gmail.com>
2024-10-30 13:04:39 +0000

    Basic includes

--- battleship.cpp
+++ battleship.cpp
@@ -1,4 +1,16 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <random>
+#include <chrono>
+#include <unordered_map>
+#include <ctime>
+
+using std::cout, std::cin, std::string, std::endl;
+
 
 int main()
 {
